27be5eb0bdbec982a6f9ad24bfbd6903869148ea
a869057476 <869057476@qq.com>
2021-03-10 21:47:39 +0800

    打出组件包

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@
  * @Author: huangwei
  * @Date: 2021-03-06 12:44:41
  * @LastEditors: huangwei
- * @LastEditTime: 2021-03-10 21:38:22
+ * @LastEditTime: 2021-03-10 21:47:01
  */
-// import Msg from './app.vue'
-// export default Msg;
 
 // import Vue from 'vue'
 // import App from './App.vue'
-// import { Checkbox } from 'element-ui'
-
-// Vue.component(Checkbox.name, Checkbox)
-
 // Vue.config.productionTip = false
-
 // new Vue({
 //   render: h => h(App),
 // }).$mount('#app')
 
+// 把这两行注释，上面的放开即可预览，不然就是打出这个组件包
 import VueTreeToTable from './TableTree/index.vue'
 export default VueTreeToTable
