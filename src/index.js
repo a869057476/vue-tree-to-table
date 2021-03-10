cf3f706bfbafa505e9eb319c7ea6b151fc7bdeaa
a869057476 <869057476@qq.com>
2021-03-10 21:49:32 +0800

    预览

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@
  * @Author: huangwei
  * @Date: 2021-03-06 12:44:41
  * @LastEditors: huangwei
- * @LastEditTime: 2021-03-10 21:47:01
+ * @LastEditTime: 2021-03-10 21:49:22
  */
 
-// import Vue from 'vue'
-// import App from './App.vue'
-// Vue.config.productionTip = false
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+import Vue from 'vue'
+import App from './App.vue'
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
 
-// 把这两行注释，上面的放开即可预览，不然就是打出这个组件包
-import VueTreeToTable from './TableTree/index.vue'
-export default VueTreeToTable
+// import VueTreeToTable from './TableTree/index.vue'
+// export default VueTreeToTable
